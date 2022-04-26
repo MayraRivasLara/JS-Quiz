144e3a7fd7f4073ebc975eeba65abeeaf152f671
Mayra Rivas Lara <mayrulara@hotmail.com>
2022-04-26 12:25:29 +0800

    Feedback comments added

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const btnStart = document.getElementById("btn-start");
 const landingPage = document.getElementById("landing-page");
 const questionPage = document.getElementById("question-page");
+const answerFeedback = document.getElementById("feedback-comment");
 const endGamePage = document.getElementById("end-game-page");
-const highscorePage = document.getElementById("highscore-page");
 const spanTimer = document.getElementById("span-timer");
 const choicesList = document.getElementById("choices-list");
 const questionTitle = document.getElementById("question-title");
@@ -10,10 +10,10 @@ const btnSubmitName = document.getElementById("btn-submit-name");
 const inputUserName = document.getElementById("input-user-name");
 const userName = document.getElementById("user-name");
 const userScore = document.getElementById("score");
-//   const highscorePage = document.getElementById("highscore-page");
-//   const highscoreList = document.getElementById("highscore-list");
+const highscorePage = document.getElementById("highscore-page");
+const highscoreList = document.getElementById("highscore-list");
 
-let timeRemaining = 45; // 45 seconds
+let timeRemaining = 60; // 60 seconds
 spanTimer.textContent = timeRemaining;
 let timerId;
 let CurrentQuestion = 0;
@@ -24,7 +24,6 @@ function checkAnswer(event) {
   event.preventDefault();
 
   // when the user clicks on the choice btn
-
   // Check if user answered correctly
 
   const isCorrect = event.target.getAttribute("data-is-correct") === "true";
@@ -33,9 +32,11 @@ function checkAnswer(event) {
 
   // if correct
   if (isCorrect) {
-    // do nothing
+    answerFeedback.textContent = "Your answer is correct!";
   } else {
     // if wrong
+    answerFeedback.textContent = "Incorrect!";
+
     // deduct the time by 10 seconds
     timeRemaining = timeRemaining - 10;
   }
@@ -104,7 +105,7 @@ function endGame() {
 }
 
 function startTimer() {
-  // timer is  --- a ticking clok
+  // timer is  --- a ticking clock
   // every passing second we decrease the clock by 1
   timerId = setInterval(function () {
     // timeRemaining--;
@@ -138,6 +139,7 @@ btnStart.addEventListener("click", function (event) {
 // when the user clicks on the submit btn
 
 btnSubmitName.addEventListener("click", function (event) {
+  event.preventDefault();
   const userInput = inputUserName.value;
   console.log(userInput);
 
@@ -145,10 +147,15 @@ btnSubmitName.addEventListener("click", function (event) {
 
   if (userInput == "") {
     // (show the error message under the input)
-    alert("Please enter your initials!");
+    alert("Please enter your name or initials!");
 
     // throw an error
     throw "empty input";
+  }
+
+  let highscoreList = JSON.parse(localStorage.getItem("highscore-list"));
+  if (highscoreList === null) {
+      highscoreList = [];
   }
 
   // save the user name to the hs list --- local storage
@@ -159,17 +166,35 @@ btnSubmitName.addEventListener("click", function (event) {
 
   // Highscore is the end game score.
   // highscore should contain user name and score.
+function displayHighScore() {
+    clearInterval(timerId);
+    timeRemaining.inputUserName = 0;
+    highscoreList.classList.remove('hide');
+    endGamePage.classList.add('hide');
 
+}
   //  Array of names + scores
 
-  // const highScore = ["userName", "score"]
+  const highScore = ["userName", "score"]
 
-  // const highScore = {userName:"Name", score:0}
-  // document.getElementById(),this.innerHTML = highScore;
+  const highScore = {userName:"Name", score:0}
+  document.getElementById(),this.innerHTML = highScore;
 
-  // show the higscore page
+  // show the highscore page
 });
 
 // highscore page
 // grab the items from local storage
 // render it as a list
+
+// user can play again by clicking play again button
+restartButton.addEventListener('click', function (event) {
+    clearInterval(timerInterval);
+    timeRemaining = 60;
+    timerElement.textContent = timeRemaining;
+    endGamePage.classList.add('hide');
+    highScores.classList.add('hide');
+    landingPage.classList.remove('hide');
+    questionIndex = 0;
+});
+
